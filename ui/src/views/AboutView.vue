b980f7ded71bfa8c2175673ccbd285ecdd05a37a
HapiFive <1143910315@qq.com>
2025-01-15 03:04:07 +0800

    使用pnpm install @element-plus/icons-vue安装图标

--- ui/src/views/AboutView.vue
+++ ui/src/views/AboutView.vue
@@ -1,5 +1,10 @@
+<script setup lang="ts">
+import { ElementPlus } from '@element-plus/icons-vue'
+</script>
+
 <template>
   <div class="about">
+    <el-icon><ElementPlus /></el-icon>
     <h1>This is an about page</h1>
   </div>
 </template>
